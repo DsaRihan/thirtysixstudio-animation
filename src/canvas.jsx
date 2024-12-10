d33af02174244e5bdae27887d5b38e44ff82b3bf
Rihan <100439488+DsaRihan@users.noreply.github.com>
2024-12-10 23:10:33 +0530

    modified src folder

--- src/canvas.jsx
+++ src/canvas.jsx
@@ -1,22 +1,41 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import image from './image'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
-const Canvas = () => {
+const Canvas = ({canvasData}) => {
+    const {startIndex, numImages, duration, size, top, left, zIndex} = canvasData;
+    const [index, setindex] = useState({value : startIndex});
     const canvasRef = useRef(null);
+
+    useGSAP(() => {
+        gsap.to(index,
+            {value : startIndex + numImages-1,
+                duration : duration,
+                ease : 'linear',repeat : -1,
+                onUpdate:()=>
+                    {setindex({value : Math.round(index.value)})}})
+    })
+
     useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');        
         const img = new Image();
-        img.src = image[0];
+        img.src = image[index.value];
         
         img.onload = () => {
             canvas.width = img.width;
             canvas.height = img.height;
             ctx.drawImage(img, 0, 0);
         };
-    })
+    },[index])
+
   return (
-    <canvas ref={canvasRef} id='canvas' className="w-[18rem] h-[18rem]"></canvas>
+    <canvas ref={canvasRef}
+        id='canvas'
+        className='absolute'
+        style={{width : `${size*1.2}px`, height : `${size*1.2}px`, top : `${top}%`, left : `${left}%`, zIndex : `${zIndex}`}}>  
+        </canvas>
   )
 }
 
